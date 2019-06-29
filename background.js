da1e19766a4fcf8774925f54b012a290892ee687
Kyle E. Mitchell <kyle@kemitchell.com>
2019-06-29 11:30:07 -0700

    Use JavaScript Standard Style

--- background.js
+++ background.js
@@ -1,104 +1,104 @@
-window.addEventListener('load', function() {
-
-  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+window.addEventListener('load', function () {
+  var width = window.innerWidth > 0 ? window.innerWidth : window.screen.width
 
   // Nobody wants to drain their cell battery rendering this.
   // The stylesheet kicks flat grey background color.
-  if (width < 800 ) return;
+  if (width < 800) return
 
   // You may remember this from CS classes I never attended.
   var shuffle = function (array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+    var currentIndex = array.length; var temporaryValue; var randomIndex
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
     }
-    return array;
+    return array
   }
 
   // Not exactly free association
   var words = [
-    'Alternative Fees'
-  , 'Asset Purchase'
-  , 'Board Meeting'
-  , 'Common Stock'
-  , 'Confidentiality'
-  , 'Conversion'
-  , 'Convertible Note'
-  , 'Data Security'
-  , 'Delaware'
-  , 'Developments'
-  , 'Digital Native'
-  , 'Earnout'
-  , 'Exclusive Provider'
-  , 'Exit Event'
-  , 'FACTA'
-  , 'Gramm-Leach-Bliley'
-  , 'HIPAA'
-  , 'Incorporation'
-  , 'Indemnification'
-  , 'Integrity'
-  , 'Intellectual Property'
-  , 'License Back'
-  , 'Licensing'
-  , 'Limited Liability Company'
-  , 'Liquidation Preference'
-  , 'Logistics'
-  , 'Mergers and Acquisitions'
-  , 'Minute Book'
-  , 'Non-Compete'
-  , 'OEM Agreement'
-  , 'Open Source'
-  , 'Option Plan'
-  , 'Outsourcing'
-  , 'Par Value'
-  , 'Pay-to-Play'
-  , 'Payment Systems'
-  , 'Plain English'
-  , 'Preferred Stock'
-  , 'Privacy Law'
-  , 'Purchase Price Adjustment'
-  , 'Reorganization'
-  , 'Reps and Warranties'
-  , 'Reseller Agreement'
-  , 'Section 368'
-  , 'Section 83(b) Election'
-  , 'Seed Round'
-  , 'Series A'
-  , 'Services Agreement'
-  , 'Silicon Valley'
-  , 'Software Integration'
-  , 'Stock Certificate'
-  , 'Restricted Stock'
-  , 'Vesting Period'
-  , 'One-Year Cliff'
-  , 'Bridge Loan'
-  , 'Stock Purchase'
-  , 'Term Sheet'
-  , 'Venture Capital'
-  , 'Waiver'
-  , 'White Label'
-  ];
+    'Alternative Fees',
+    'Asset Purchase',
+    'Board Meeting',
+    'Common Stock',
+    'Confidentiality',
+    'Conversion',
+    'Convertible Note',
+    'Data Security',
+    'Delaware',
+    'Developments',
+    'Digital Native',
+    'Earnout',
+    'Exclusive Provider',
+    'Exit Event',
+    'FACTA',
+    'Gramm-Leach-Bliley',
+    'HIPAA',
+    'Incorporation',
+    'Indemnification',
+    'Integrity',
+    'Intellectual Property',
+    'License Back',
+    'Licensing',
+    'Limited Liability Company',
+    'Liquidation Preference',
+    'Logistics',
+    'Mergers and Acquisitions',
+    'Minute Book',
+    'Non-Compete',
+    'OEM Agreement',
+    'Open Source',
+    'Option Plan',
+    'Outsourcing',
+    'Par Value',
+    'Pay-to-Play',
+    'Payment Systems',
+    'Plain English',
+    'Preferred Stock',
+    'Privacy Law',
+    'Purchase Price Adjustment',
+    'Reorganization',
+    'Reps and Warranties',
+    'Reseller Agreement',
+    'Section 368',
+    'Section 83(b) Election',
+    'Seed Round',
+    'Series A',
+    'Services Agreement',
+    'Silicon Valley',
+    'Software Integration',
+    'Stock Certificate',
+    'Restricted Stock',
+    'Vesting Period',
+    'One-Year Cliff',
+    'Bridge Loan',
+    'Stock Purchase',
+    'Term Sheet',
+    'Venture Capital',
+    'Waiver',
+    'White Label'
+  ]
 
   var appendWords = function (words, rounds) {
     // Thank you, John Resig.
-    var bg = document.getElementById('background'),
-    fragment = document.createDocumentFragment();
+    var bg = document.getElementById('background')
+
+    var fragment = document.createDocumentFragment()
     for (var round = 0; round < rounds; round++) {
-      shuffle(words);
-      for(var i = 0; i < words.length; i++) {
-      fragment.appendChild(
-        // Non-breaking and a regular space.
-        document.createTextNode(words[i] + "  ")
-      );
+      shuffle(words)
+      for (var i = 0; i < words.length; i++) {
+        fragment.appendChild(
+          // Non-breaking and a regular space.
+          document.createTextNode(words[i] + '  ')
+        )
       }
     }
-    bg.appendChild(fragment.cloneNode(true));
-  };
+    bg.appendChild(fragment.cloneNode(true))
+  }
 
   // Quintuple-stuffed!
-  appendWords(words, 5);
-});
+  appendWords(words, 5)
+})
